4609396ad943f13a764212383dc425f57121420c
CatDeveloper <maks.rozhkov.2004@gmail.com>
2023-09-08 22:58:01 +0500

    fix: delete footer

--- src/components/HomeLayout.jsx
+++ src/components/HomeLayout.jsx
@@ -13,7 +13,7 @@ const HomeLayout = () => {
             <WidthContent>
                 <Outlet/>
             </WidthContent>
-            <Footer/>
+            {/*<Footer/>*/}
         </div>
     );
 };
